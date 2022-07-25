4249eb6d25805c87b5a6c9d0d13eee2460ffcc22
leliu1224 <leliu08640@gmail.com>
2022-07-24 21:35:47 -0400

    fix runewords

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ const Header = ({ siteTitle }) => (
   <header>
     <div>
       <h1>{siteTitle}</h1>
-      <small>Up to date with version 2.2</small>
+      <small>Up to date with version 2.3</small>
     </div>
   </header>
 )
