2c9a05e9398651de6095e009e1c70771a259437d
leliu1224 <leliu08640@gmail.com>
2023-12-26 22:49:04 -0500

    update to 2.8

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ const Header = ({ siteTitle }) => (
   <header>
     <div>
       <h1>{siteTitle}</h1>
-      <small>Up to date with version 2.6</small>
+      <small>Up to date with version 2.8</small>
     </div>
   </header>
 );
